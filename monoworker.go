7c18702850e4c5276db95ff59116a38590e84fe6
girvel <widauka@ya.ru>
2025-06-21 17:09:06 +0500

    Id handling with no race conditions

--- monoworker.go
+++ monoworker.go
@@ -7,27 +7,43 @@ import (
     "time"
     "math/rand/v2"
     "sync"
-    "sync/atomic"
 
     "github.com/gin-gonic/gin"
 )
 
+type Task struct {
+    target string
+    id int
+}
+
+func createTask(in chan Task, target string) (int, bool) {
+    lastIdLock.Lock()
+    defer lastIdLock.Unlock()
+
+    select {
+    case in <- Task{target, lastId + 1}:
+        lastId++
+        return lastId, true
+    default:
+        return -1, false
+    }
+}
+
 var results map[int]string
 var resultsLock sync.Mutex
-var lastId atomic.Int32
+var lastId int = -1
+var lastIdLock sync.Mutex
 
-func say_hello(target string, id int) {
+func say_hello(task Task) {
     time.Sleep(time.Second * time.Duration(50 + rand.IntN(20)))
     resultsLock.Lock()
-    results[id] = fmt.Sprintf("Hello, %s!", target)
+    results[task.id] = fmt.Sprintf("Hello, %s!", task.target)
     resultsLock.Unlock()
 }
 
-func worker(in chan string) {
+func worker(in chan Task) {
     for {
-        target := <-in
-        lastId.Add(1)
-        go say_hello(target, int(lastId.Load()))
+        go say_hello(<-in)
     }
 }
 
@@ -37,9 +53,8 @@ type CreateRequest struct {
 
 func main() {
     results = make(map[int]string)
-    lastId.Store(-1)
 
-    in := make(chan string, 1024)
+    in := make(chan Task, 1024)
     go worker(in)
 
     g := gin.Default()
@@ -57,12 +72,9 @@ func main() {
             return
         }
 
-        select {
-        case in <- json.Target:
-            c.JSON(http.StatusOK, gin.H {
-                "id": lastId.Load() + 1,
-            })
-        default:
+        if id, ok := createTask(in, json.Target); ok {
+            c.JSON(http.StatusOK, gin.H{"id": id})
+        } else {
             c.JSON(http.StatusServiceUnavailable, gin.H {"error": "system busy"})
         }
     })
@@ -70,7 +82,7 @@ func main() {
     g.GET("/task", func (c *gin.Context) {
         c.JSON(http.StatusOK, gin.H {
             "ready": len(results),
-            "in_progress": int(lastId.Load()) - len(results) + 1,
+            "in_progress": lastId - len(results) + 1,
         })
     })
 
@@ -83,7 +95,7 @@ func main() {
 
         var status string
         switch {
-        case id > int(lastId.Load()):
+        case id > lastId:
             status = "non_existent"
         case id >= len(results):
             status = "in_progress"

7a9aaf8eb0ae9677356c9528fdba280716226e7f
girvel <widauka@ya.ru>
2025-06-23 16:49:25 +0500

    Extract src/api.go

--- monoworker.go
+++ monoworker.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"strconv"
     "time"
     "math/rand/v2"
 
-	"github.com/gin-gonic/gin"
 	monoworker "github.com/girvel/monoworker/src"
 )
 
@@ -16,69 +13,8 @@ func say_hello(input string) string {
     return fmt.Sprintf("Hello, %s!", input)
 }
 
-type CreateRequest struct {
-    Target string `json:"target"`
-}
-
 func main() {
     worker := monoworker.NewWorker(say_hello)
     go worker.Run()
-
-    g := gin.Default()
-
-    g.GET("/ping", func (c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H {
-            "result": "pong",
-        })
-    })
-
-    g.POST("/task", func (c *gin.Context) {
-        var json CreateRequest
-        if err := c.ShouldBindJSON(&json); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H {"error": err.Error()})
-            return
-        }
-
-        if id, ok := worker.CreateTask(json.Target); ok {
-            c.JSON(http.StatusOK, gin.H{"id": id})
-        } else {
-            c.JSON(http.StatusServiceUnavailable, gin.H {"error": "system busy"})
-        }
-    })
-
-    g.GET("/task", func (c *gin.Context) {
-        c.JSON(http.StatusOK, worker.GetStats())
-    })
-
-    g.GET("/task/:id", func (c *gin.Context) {
-        id, err := strconv.Atoi(c.Param("id"))
-        if err != nil {
-            c.JSON(http.StatusBadRequest, gin.H {"error": "id should be a number"})
-            return
-        }
-
-        c.JSON(http.StatusOK, gin.H {
-            "status": worker.GetTaskStatus(id),
-        })
-    })
-
-    g.GET("/result/:id", func(c *gin.Context) {
-        id, err := strconv.Atoi(c.Param("id"))
-        if err != nil {
-            c.JSON(http.StatusBadRequest, gin.H {"error": "id should be a number"})
-            return
-        }
-
-        if result, exists := worker.GetTaskResult(id); exists {
-            c.JSON(http.StatusOK, gin.H {
-                "result": result,
-            })
-        } else {
-            c.JSON(http.StatusNotFound, gin.H {
-                "error": fmt.Sprintf("no result with id %d", id),
-            })
-        }
-    })
-
-    g.Run()
+    monoworker.RunAPI(worker)
 }
---
+++ src/api.go
@@ -0,0 +1,73 @@
+package monoworker
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CreateRequest struct {
+    Target string `json:"target"`
+}
+
+func RunAPI(worker *Worker[string, string]) {
+    g := gin.Default()
+
+    g.GET("/ping", func (c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H {
+            "result": "pong",
+        })
+    })
+
+    g.POST("/task", func (c *gin.Context) {
+        var json CreateRequest
+        if err := c.ShouldBindJSON(&json); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H {"error": err.Error()})
+            return
+        }
+
+        if id, ok := worker.CreateTask(json.Target); ok {
+            c.JSON(http.StatusOK, gin.H{"id": id})
+        } else {
+            c.JSON(http.StatusServiceUnavailable, gin.H {"error": "system busy"})
+        }
+    })
+
+    g.GET("/task", func (c *gin.Context) {
+        c.JSON(http.StatusOK, worker.GetStats())
+    })
+
+    g.GET("/task/:id", func (c *gin.Context) {
+        id, err := strconv.Atoi(c.Param("id"))
+        if err != nil {
+            c.JSON(http.StatusBadRequest, gin.H {"error": "id should be a number"})
+            return
+        }
+
+        c.JSON(http.StatusOK, gin.H {
+            "status": worker.GetTaskStatus(id),
+        })
+    })
+
+    g.GET("/result/:id", func(c *gin.Context) {
+        id, err := strconv.Atoi(c.Param("id"))
+        if err != nil {
+            c.JSON(http.StatusBadRequest, gin.H {"error": "id should be a number"})
+            return
+        }
+
+        if result, exists := worker.GetTaskResult(id); exists {
+            c.JSON(http.StatusOK, gin.H {
+                "result": result,
+            })
+        } else {
+            c.JSON(http.StatusNotFound, gin.H {
+                "error": fmt.Sprintf("no result with id %d", id),
+            })
+        }
+    })
+
+    g.Run()
+}
